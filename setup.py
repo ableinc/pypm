145b434b7f3588ed0e7bf306189c6616cdd89f98
Douglas, Jaylen <Jdougl24@its.jnj.com>
2019-11-13 09:29:20 -0500

    #BugFix. Wrong function being called when CLI tool activated.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     ],
     entry_points='''
         [console_scripts]
-        pypm=pypm.cli:__main__
+        pypm=pypm.cli:main
     ''',
     install_requires=dependencies,
     classifiers=[
